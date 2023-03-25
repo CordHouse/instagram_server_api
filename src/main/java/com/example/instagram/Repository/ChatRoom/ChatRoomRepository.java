df7a1164caf68ef2a28fdb90ef9a27be9be71ca6
CordHouse <qkqktl5310@gmail.com>
2023-03-26 04:13:59 +0900

    feat : 채팅방 Repo init

---
+++ src/main/java/com/example/instagram/Repository/ChatRoom/ChatRoomRepository.java
@@ -0,0 +1,14 @@
+package com.example.instagram.Repository.ChatRoom;
+
+import com.example.instagram.Entity.ChatRoom.ChatRoom;
+import com.example.instagram.Entity.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    ChatRoom findByHostAndUserOrUserAndHost(long id, User user, User user1, long id1);
+    List<ChatRoom> findAllByHostOrUser_Id(long host, long user);
+}
