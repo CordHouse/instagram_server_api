c59e724c38723312757ec27a7262787cf5c592d0
CordHouse <qkqktl5310@gmail.com>
2023-03-26 04:30:41 +0900

    refactor : Board -> Posts

--- src/main/java/com/example/instagram/Dto/Posts/PostsCreateRequestDto.java
+++ src/main/java/com/example/instagram/Dto/Posts/PostsCreateRequestDto.java
@@ -1,4 +1,4 @@
-package com.example.instagram.Dto.Board;
+package com.example.instagram.Dto.Posts;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,7 +9,7 @@ import javax.validation.constraints.NotBlank;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class BoardCreateRequestDto {
+public class PostsCreateRequestDto {
     @NotBlank(message = "이미지를 업로드 해주세요.")
     private String image;
     @NotBlank(message = "게시물 내용을 입력해주세요.")
