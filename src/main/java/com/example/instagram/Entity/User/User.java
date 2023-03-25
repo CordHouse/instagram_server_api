1478cb09daf18ef09488bd3273ae7bec7be6fbfe
CordHouse <qkqktl5310@gmail.com>
2023-03-26 04:38:25 +0900

    feat : User entity dto

---
+++ src/main/java/com/example/instagram/Entity/User/User.java
@@ -0,0 +1,36 @@
+package com.example.instagram.Entity.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String profile_image_url;
+
+    @Column(nullable = false)
+    private long follow;
+
+    @Column(nullable = false)
+    private long following;
+
+    public User(String nickname, String profile_image_url) {
+        this.nickname = nickname;
+        this.profile_image_url = profile_image_url;
+        this.follow = 0;
+        this.following = 0;
+    }
+}
