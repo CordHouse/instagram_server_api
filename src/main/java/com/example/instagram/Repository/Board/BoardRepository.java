29343d0fc91e1a946e5cd3d62ba23e5b9c7d6947
CordHouse <qkqktl5310@gmail.com>
2023-03-26 04:01:37 +0900

    docs : Board -> Posts

--- src/main/java/com/example/instagram/Repository/Board/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.instagram.Repository.Board;
-
-import com.example.instagram.Entity.Board.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
