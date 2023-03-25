14c29e56c71e50d6ca284a3f7c4051d7205fe038
CordHouse <qkqktl5310@gmail.com>
2023-03-26 04:40:20 +0900

    feat : user repo init

---
+++ src/main/java/com/example/instagram/Repository/User/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.instagram.Repository.User;
+
+import com.example.instagram.Entity.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
