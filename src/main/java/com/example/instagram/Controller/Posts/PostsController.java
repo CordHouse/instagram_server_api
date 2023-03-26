e64cc16fa946650bafa4b3550016eb4bd77b7133
CordHouse <qkqktl5310@gmail.com>
2023-03-27 05:13:50 +0900

    refactor : path -> board -> url feat : 피드 조회 추가

--- src/main/java/com/example/instagram/Controller/Posts/PostsController.java
+++ src/main/java/com/example/instagram/Controller/Posts/PostsController.java
@@ -1,5 +1,6 @@
 package com.example.instagram.Controller.Posts;
 
+import com.example.instagram.Dto.Feed.FeedResponseDto;
 import com.example.instagram.Dto.Posts.PostsCreateRequestDto;
 import com.example.instagram.Dto.Posts.PostsCreateResponseDto;
 import com.example.instagram.Dto.Posts.PostsEditRequestDto;
@@ -18,23 +19,30 @@ public class PostsController {
     private final PostsService postsService;
 
     // 글 생성
-    @PostMapping("/board")
+    @PostMapping("/posts")
     @ResponseStatus(HttpStatus.OK)
     public PostsCreateResponseDto createBoard(@RequestBody @Valid PostsCreateRequestDto postsCreateRequestDto) {
         return postsService.createBoard(postsCreateRequestDto);
     }
 
     // 글 수정
-    @PutMapping("/board/{id}")
+    @PutMapping("/posts/{id}")
     @ResponseStatus(HttpStatus.OK)
     public PostsEditResponseDto editBoard(@PathVariable long id, @RequestBody @Valid PostsEditRequestDto postsEditRequestDto) {
         return postsService.editBoard(id, postsEditRequestDto);
     }
 
     // 글 삭제
-    @DeleteMapping("/board/{id}")
+    @DeleteMapping("/posts/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteBoard(@PathVariable long id) {
         postsService.deleteBoard(id);
+    }
+
+    // 피드 조회
+    @GetMapping("/posts/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public FeedResponseDto getFeed(@PathVariable long id) {
+        return postsService.getFeed(id);
     }
 }
