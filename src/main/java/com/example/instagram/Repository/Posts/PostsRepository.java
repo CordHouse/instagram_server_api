083d86962978d530d669ebcf4d968859996b00fd
CordHouse <qkqktl5310@gmail.com>
2023-03-27 05:14:13 +0900

    feat : 유저 id를 기준으로 조회

--- src/main/java/com/example/instagram/Repository/Posts/PostsRepository.java
+++ src/main/java/com/example/instagram/Repository/Posts/PostsRepository.java
@@ -4,6 +4,9 @@ import com.example.instagram.Entity.Posts.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    List<Posts> findAllByUser_Id(long id);
 }
