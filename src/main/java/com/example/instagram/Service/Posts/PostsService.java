de58f157154f9567068fb28331ce0bc67992edfd
CordHouse <qkqktl5310@gmail.com>
2023-03-27 05:14:36 +0900

    feat : 피드 조회 추가

--- src/main/java/com/example/instagram/Service/Posts/PostsService.java
+++ src/main/java/com/example/instagram/Service/Posts/PostsService.java
@@ -1,17 +1,21 @@
 package com.example.instagram.Service.Posts;
 
+import com.example.instagram.Dto.Feed.FeedResponseDto;
 import com.example.instagram.Dto.Posts.PostsCreateRequestDto;
 import com.example.instagram.Dto.Posts.PostsCreateResponseDto;
 import com.example.instagram.Dto.Posts.PostsEditRequestDto;
 import com.example.instagram.Dto.Posts.PostsEditResponseDto;
 import com.example.instagram.Entity.Posts.Posts;
 import com.example.instagram.Entity.User.User;
+import com.example.instagram.Exception.Posts.NotFoundFeedException;
 import com.example.instagram.Exception.Posts.NotFoundPostsException;
 import com.example.instagram.Repository.Posts.PostsRepository;
 import com.example.instagram.Repository.User.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +29,7 @@ public class PostsService {
         User user = userRepository.findById(1L).orElseThrow();
         Posts newPosts = new Posts(user, postsCreateRequestDto.getImage(), postsCreateRequestDto.getContent());
         postsRepository.save(newPosts);
+
         return new PostsCreateResponseDto().toDo(newPosts);
     }
 
@@ -41,5 +46,15 @@ public class PostsService {
     @Transactional
     public void deleteBoard(long id) {
         postsRepository.deleteById(id);
+    }
+
+    // 피드 조회
+    @Transactional
+    public FeedResponseDto getFeed(long id) {
+        List<Posts> feedList = postsRepository.findAllByUser_Id(id);
+        if(feedList.isEmpty()) {
+            throw new NotFoundFeedException();
+        }
+        return new FeedResponseDto().toDo(feedList);
     }
 }
