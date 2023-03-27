38521f4fd8a39c7ea373e0646a8007541735a974
CordHouse <qkqktl5310@gmail.com>
2023-03-27 22:53:59 +0900

    feat : findByIdAndUser deleteByIdAndUser 추가

--- src/main/java/com/example/instagram/Repository/Posts/PostsRepository.java
+++ src/main/java/com/example/instagram/Repository/Posts/PostsRepository.java
@@ -1,12 +1,16 @@
 package com.example.instagram.Repository.Posts;
 
 import com.example.instagram.Entity.Posts.Posts;
+import com.example.instagram.Entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     List<Posts> findAllByUser_Id(long id);
+    Optional<Posts> findByIdAndUser(long id, User user);
+    void deleteByIdAndUser(long id, User user);
 }
