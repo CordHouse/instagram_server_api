ca403cd7b589aeb7674ebb081505b428ac2b1cb1
CordHouse <qkqktl5310@gmail.com>
2023-03-27 22:52:51 +0900

    refactor : token 에 있는 유저 객체로 변경 피드 조회 path 수정

--- src/main/java/com/example/instagram/Controller/Posts/PostsController.java
+++ src/main/java/com/example/instagram/Controller/Posts/PostsController.java
@@ -5,9 +5,14 @@ import com.example.instagram.Dto.Posts.PostsCreateRequestDto;
 import com.example.instagram.Dto.Posts.PostsCreateResponseDto;
 import com.example.instagram.Dto.Posts.PostsEditRequestDto;
 import com.example.instagram.Dto.Posts.PostsEditResponseDto;
+import com.example.instagram.Entity.User.User;
+import com.example.instagram.Exception.User.NotFoundUserException;
+import com.example.instagram.Repository.User.UserRepository;
 import com.example.instagram.Service.Posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,32 +22,39 @@ import javax.validation.Valid;
 @RequestMapping("/api")
 public class PostsController {
     private final PostsService postsService;
+    private final UserRepository userRepository;
 
     // 글 생성
     @PostMapping("/posts")
     @ResponseStatus(HttpStatus.OK)
     public PostsCreateResponseDto createBoard(@RequestBody @Valid PostsCreateRequestDto postsCreateRequestDto) {
-        return postsService.createBoard(postsCreateRequestDto);
+        return postsService.createBoard(postsCreateRequestDto, getUser());
     }
 
     // 글 수정
     @PutMapping("/posts/{id}")
     @ResponseStatus(HttpStatus.OK)
     public PostsEditResponseDto editBoard(@PathVariable long id, @RequestBody @Valid PostsEditRequestDto postsEditRequestDto) {
-        return postsService.editBoard(id, postsEditRequestDto);
+        return postsService.editBoard(id, postsEditRequestDto, getUser());
     }
 
     // 글 삭제
     @DeleteMapping("/posts/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteBoard(@PathVariable long id) {
-        postsService.deleteBoard(id);
+        postsService.deleteBoard(id, getUser());
     }
 
     // 피드 조회
-    @GetMapping("/posts/{id}")
+    @GetMapping("/posts")
     @ResponseStatus(HttpStatus.OK)
-    public FeedResponseDto getFeed(@PathVariable long id) {
-        return postsService.getFeed(id);
+    public FeedResponseDto getFeed() {
+        return postsService.getFeed(getUser());
+    }
+
+    // 토큰 정보로 유저 객체 생성
+    public User getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findByNickname(authentication.getName()).orElseThrow(NotFoundUserException::new);
     }
 }
