4f405cdd36c1d93e6fa96ede45ab893f87fb9928
CordHouse <qkqktl5310@gmail.com>
2023-03-27 22:02:51 +0900

    feat : 로그인, 토큰 재발급, 토큰을 통한 User 객체 생성

--- src/main/java/com/example/instagram/Controller/User/UserController.java
+++ src/main/java/com/example/instagram/Controller/User/UserController.java
@@ -1,9 +1,16 @@
 package com.example.instagram.Controller.User;
 
+import com.example.instagram.Dto.Token.TokenReissueRequestDto;
+import com.example.instagram.Dto.Token.TokenResponseDto;
 import com.example.instagram.Dto.User.*;
+import com.example.instagram.Entity.User.User;
+import com.example.instagram.Exception.User.NotFoundUserException;
+import com.example.instagram.Repository.User.UserRepository;
 import com.example.instagram.Service.User.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -13,6 +20,7 @@ import javax.validation.Valid;
 @RequestMapping("/api")
 public class UserController {
     private final UserService userService;
+    private final UserRepository userRepository;
 
     // 회원가입
     @PostMapping("/register")
@@ -28,6 +36,20 @@ public class UserController {
         userService.deleteUser(id);
     }
 
+    // 로그인
+    @PostMapping("/signIn")
+    @ResponseStatus(HttpStatus.OK)
+    public TokenResponseDto signIn(@RequestBody @Valid UserSignInRequestDto userSignInRequestDto) {
+        return userService.signIn(userSignInRequestDto);
+    }
+
+    // 토큰 재발급
+    @PostMapping("/reissue")
+    @ResponseStatus(HttpStatus.OK)
+    public TokenResponseDto tokenReissue(@RequestBody @Valid TokenReissueRequestDto tokenReissueRequestDto) {
+        return userService.tokenReissue(tokenReissueRequestDto, getUser());
+    }
+
     // 프로필 조회 -> Jwt + security 적용 이후 확인
     @PostMapping("/profile/{id}")
     @ResponseStatus(HttpStatus.OK)
@@ -40,5 +62,11 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     public UserEditResponseDto editProfile(@RequestBody @Valid UserEditRequestDto userEditRequestDto) {
         return userService.editProfile(userEditRequestDto);
+    }
+
+    // 토큰 정보로 유저 객체 생성
+    public User getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findByNickname(authentication.getName()).orElseThrow(NotFoundUserException::new);
     }
 }
