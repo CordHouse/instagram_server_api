ea3fdcd58d6c55205ceac5078f3a11ea782fcdd6
CordHouse <qkqktl5310@gmail.com>
2023-03-27 22:21:52 +0900

    refactor : 토큰정보에서 User 객체를 찾도록 변경

--- src/main/java/com/example/instagram/Controller/Follow/FollowController.java
+++ src/main/java/com/example/instagram/Controller/Follow/FollowController.java
@@ -2,9 +2,14 @@ package com.example.instagram.Controller.Follow;
 
 import com.example.instagram.Dto.Follow.FollowRequestDto;
 import com.example.instagram.Dto.Follow.UnFollowRequestDto;
+import com.example.instagram.Entity.User.User;
+import com.example.instagram.Exception.User.NotFoundUserException;
+import com.example.instagram.Repository.User.UserRepository;
 import com.example.instagram.Service.Follow.FollowService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,18 +19,25 @@ import javax.validation.Valid;
 @RequestMapping("/api")
 public class FollowController {
     private final FollowService followService;
+    private final UserRepository userRepository;
 
     // 팔로우
     @PostMapping("/follow")
     @ResponseStatus(HttpStatus.OK)
     public void follow(@RequestBody @Valid FollowRequestDto followRequestDto) {
-        followService.follow(followRequestDto.getUser_id());
+        followService.follow(followRequestDto.getUser_id(), getUser());
     }
 
     // 팔로우 취소
     @DeleteMapping("/follow")
     @ResponseStatus(HttpStatus.OK)
     public void unFollow(@RequestBody @Valid UnFollowRequestDto unFollowRequestDto) {
-        followService.unFollow(unFollowRequestDto.getUser_id());
+        followService.unFollow(unFollowRequestDto.getUser_id(), getUser());
+    }
+
+    // 토큰 정보로 유저 객체 생성
+    public User getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findByNickname(authentication.getName()).orElseThrow(NotFoundUserException::new);
     }
 }
--- src/main/java/com/example/instagram/Service/Follow/FollowService.java
+++ src/main/java/com/example/instagram/Service/Follow/FollowService.java
@@ -18,11 +18,11 @@ public class FollowService {
 
     // 팔로우 신청
     @Transactional
-    public void follow(long id) {
-        User followRequestUser = userRepository.findById(1L).orElseThrow(NotFoundUserException::new);
+    public void follow(long id, User user) {
+        User followRequestUser = userRepository.findById(user.getId()).orElseThrow(NotFoundUserException::new);
         User followResponseUser = userRepository.findById(id).orElseThrow(NotFoundUserException::new);
 
-        Follow newFollow = followRepository.findByHostAndUser(1L, followResponseUser).orElseThrow(AlreadyFollowException::new);
+        Follow newFollow = followRepository.findByHostAndUser(user.getId(), followResponseUser).orElseThrow(AlreadyFollowException::new);
         followRequestUser.setFollow(followRequestUser.getFollow()+1);
         followResponseUser.setFollowing(followResponseUser.getFollowing()+1);
         followRepository.save(newFollow);
@@ -30,9 +30,10 @@ public class FollowService {
 
     // 팔로우 취소
     @Transactional
-    public void unFollow(long id) {
-        User followRequestUser = userRepository.findById(1L).orElseThrow(NotFoundUserException::new);
+    public void unFollow(long id, User user) {
+        User followRequestUser = userRepository.findById(user.getId()).orElseThrow(NotFoundUserException::new);
         User followResponseUser = userRepository.findById(id).orElseThrow(NotFoundUserException::new);
+
         followRequestUser.setFollow(followRequestUser.getFollow()-1);
         followResponseUser.setFollowing(followResponseUser.getFollowing()-1);
         followRepository.deleteById(id);
