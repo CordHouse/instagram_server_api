8f138504483e319b151dc81f66247633daf62493
CordHouse <qkqktl5310@gmail.com>
2023-03-27 22:13:24 +0900

    refactor : 댓글 pk와 유저 id를 통해 유저 찾기

--- src/main/java/com/example/instagram/Repository/Comment/CommentRepository.java
+++ src/main/java/com/example/instagram/Repository/Comment/CommentRepository.java
@@ -1,9 +1,13 @@
 package com.example.instagram.Repository.Comment;
 
 import com.example.instagram.Entity.Comment.Comment;
+import com.example.instagram.Entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findByIdAndUser(long id, User user);
 }
