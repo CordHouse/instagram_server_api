4c3e24fc515705982e7eab81c9d275973f73cc7e
CordHouse <qkqktl5310@gmail.com>
2023-03-27 22:54:52 +0900

    refactor : token 에 있는 User 객체로 변경

--- src/main/java/com/example/instagram/Service/Replies/RepliesService.java
+++ src/main/java/com/example/instagram/Service/Replies/RepliesService.java
@@ -9,10 +9,8 @@ import com.example.instagram.Entity.Replies.Replies;
 import com.example.instagram.Entity.User.User;
 import com.example.instagram.Exception.Comment.NotFoundCommentException;
 import com.example.instagram.Exception.Replies.NotFoundRepliesException;
-import com.example.instagram.Exception.User.NotFoundUserException;
 import com.example.instagram.Repository.Comment.CommentRepository;
 import com.example.instagram.Repository.Replies.RepliesRepository;
-import com.example.instagram.Repository.User.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,12 +20,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class RepliesService {
     private final RepliesRepository repliesRepository;
     private final CommentRepository commentRepository;
-    private final UserRepository userRepository;
 
     // 답글 생성
     @Transactional
-    public RepliesCreateResponseDto createReplies(RepliesCreateRequestDto repliesCreateRequestDto) {
-        User user = userRepository.findById(1L).orElseThrow(NotFoundUserException::new);
+    public RepliesCreateResponseDto createReplies(RepliesCreateRequestDto repliesCreateRequestDto, User user) {
         Comment choiceComment = commentRepository.findById(repliesCreateRequestDto.getComment_id())
                 .orElseThrow(NotFoundCommentException::new);
         Replies newReplies = new Replies(repliesCreateRequestDto.getContent(), choiceComment, user);
@@ -37,8 +33,8 @@ public class RepliesService {
 
     // 답글 수정
     @Transactional
-    public RepliesEditResponseDto editReplies(RepliesEditRequestDto repliesEditRequestDto) {
-        Replies editReplies = repliesRepository.findById(repliesEditRequestDto.getId())
+    public RepliesEditResponseDto editReplies(RepliesEditRequestDto repliesEditRequestDto, User user) {
+        Replies editReplies = repliesRepository.findByIdAndUser(repliesEditRequestDto.getId(), user)
                 .orElseThrow(NotFoundRepliesException::new);
         editReplies.setContent(repliesEditRequestDto.getContent());
         return new RepliesEditResponseDto().toDo(editReplies);
@@ -46,7 +42,7 @@ public class RepliesService {
 
     // 답글 삭제
     @Transactional
-    public void deleteReplies(long id) {
-        repliesRepository.deleteById(id);
+    public void deleteReplies(long id, User user) {
+        repliesRepository.deleteByIdAndUser(id, user);
     }
 }
