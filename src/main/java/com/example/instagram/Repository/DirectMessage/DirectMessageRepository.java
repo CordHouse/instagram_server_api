4a2d1d23c5339347fb6d23b45010b4ea134b0161
CordHouse <qkqktl5310@gmail.com>
2023-03-27 22:51:11 +0900

    refactor : sender or target 유저가 해당 되는 DM인지 조회

--- src/main/java/com/example/instagram/Repository/DirectMessage/DirectMessageRepository.java
+++ src/main/java/com/example/instagram/Repository/DirectMessage/DirectMessageRepository.java
@@ -1,6 +1,7 @@
 package com.example.instagram.Repository.DirectMessage;
 
 import com.example.instagram.Entity.DirectMessage.DirectMessage;
+import com.example.instagram.Entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface DirectMessageRepository extends JpaRepository<DirectMessage, Long> {
-    List<DirectMessage> findAllByChatRoom_Id(long id);
+    List<DirectMessage> findAllBySenderOrUser(User sender, User receiver);
 }
