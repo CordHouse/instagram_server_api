c55df1c2d49a08d344ae779d2f81095eb8b5f294
CordHouse <qkqktl5310@gmail.com>
2023-03-29 20:07:50 +0900

    feat : id 추가

--- src/main/java/com/example/instagram/Dto/Posts/PostsEditRequestDto.java
+++ src/main/java/com/example/instagram/Dto/Posts/PostsEditRequestDto.java
@@ -5,11 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostsEditRequestDto {
+    @NotNull(message = "수정할 게시글을 선택해주세요.")
+    private long id;
     @NotBlank(message = "이미지를 업로드 해주세요.")
     private String image;
     @NotBlank(message = "수정할 내용을 입력해주세요.")
