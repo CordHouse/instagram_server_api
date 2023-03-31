763a275291e7d023c562f8e0d7b52d0f6ab32e85
CordHouse <qkqktl5310@gmail.com>
2023-03-31 20:25:17 +0900

    refactor : 옵셔널 제거, long -> User로 변경

--- src/main/java/com/example/instagram/Repository/Follow/FollowRepository.java
+++ src/main/java/com/example/instagram/Repository/Follow/FollowRepository.java
@@ -5,9 +5,7 @@ import com.example.instagram.Entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
-    Optional<Follow> findByHostAndUser(long id, User user);
+    Follow findBySenderAndReceiver(User send, User receiver);
 }
