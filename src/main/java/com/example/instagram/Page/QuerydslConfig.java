2938006bcc903fdddc4420231ca5d2b15a46069b
CordHouse <qkqktl5310@gmail.com>
2023-04-07 22:54:17 +0900

    style : 경로 변경

--- src/main/java/com/example/instagram/Page/QuerydslConfig.java
+++ src/main/java/com/example/instagram/Page/QuerydslConfig.java
@@ -1,4 +1,4 @@
-package com.example.instagram.Page.QueryDsl;
+package com.example.instagram.Page;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
