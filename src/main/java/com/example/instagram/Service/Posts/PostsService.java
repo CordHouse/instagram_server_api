e4146183f5de25a1e8d0e54fcf5c6f80c558b9f7
CordHouse <qkqktl5310@gmail.com>
2023-04-07 17:05:49 +0900

    refactor : 글 삭제 -> Dto 추가 피드 조회 -> Pageable 추가, Dto 추가, User객체 삭제

--- src/main/java/com/example/instagram/Service/Posts/PostsService.java
+++ src/main/java/com/example/instagram/Service/Posts/PostsService.java
@@ -1,20 +1,18 @@
 package com.example.instagram.Service.Posts;
 
+import com.example.instagram.Dto.Feed.FeedRequestDto;
 import com.example.instagram.Dto.Feed.FeedResponseDto;
-import com.example.instagram.Dto.Posts.PostsCreateRequestDto;
-import com.example.instagram.Dto.Posts.PostsCreateResponseDto;
-import com.example.instagram.Dto.Posts.PostsEditRequestDto;
-import com.example.instagram.Dto.Posts.PostsEditResponseDto;
+import com.example.instagram.Dto.Posts.*;
 import com.example.instagram.Entity.Posts.Posts;
 import com.example.instagram.Entity.User.User;
 import com.example.instagram.Exception.Posts.NotFoundFeedException;
 import com.example.instagram.Exception.Posts.NotFoundPostsException;
 import com.example.instagram.Repository.Posts.PostsRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -45,14 +43,14 @@ public class PostsService {
 
     // 글 삭제
     @Transactional
-    public void deleteBoard(long id, User user) {
-        postsRepository.deleteByIdAndUser(id, user);
+    public void deleteBoard(PostDeleteRequestDto postDeleteRequestDto, User user) {
+        postsRepository.deleteByIdAndUser(postDeleteRequestDto.getId(), user);
     }
 
     // 피드 조회
     @Transactional
-    public FeedResponseDto getFeed(User user) {
-        List<Posts> feedList = postsRepository.findAllByUser_Id(user.getId());
+    public FeedResponseDto getFeed(Pageable pageable, FeedRequestDto feedRequestDto) {
+        Page<Posts> feedList = postsRepository.findByQUserPostsCustomCursorPaging(pageable, feedRequestDto.getUser_id(), feedRequestDto.getCursor());
         if(feedList.isEmpty()) {
             throw new NotFoundFeedException();
         }
