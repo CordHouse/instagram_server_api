59b14dbdd760bc028851bde7b1431dde166c9602
CordHouse <qkqktl5310@gmail.com>
2023-04-07 16:54:40 +0900

    refactor : List -> Page

--- src/main/java/com/example/instagram/Dto/Feed/FeedResponseDto.java
+++ src/main/java/com/example/instagram/Dto/Feed/FeedResponseDto.java
@@ -4,6 +4,7 @@ import com.example.instagram.Entity.Posts.Posts;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,7 +15,7 @@ import java.util.stream.Collectors;
 public class FeedResponseDto {
     private List<FeedPostsResponseDto> posts;
 
-    public FeedResponseDto toDo(List<Posts> posts) {
+    public FeedResponseDto toDo(Page<Posts> posts) {
         return new FeedResponseDto(
                 posts.stream().map(post -> new FeedPostsResponseDto().toDo(post)).collect(Collectors.toList())
         );
