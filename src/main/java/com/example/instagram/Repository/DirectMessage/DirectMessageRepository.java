c437ed63eed9e5d38270d43072fab0d0418af91f
CordHouse <qkqktl5310@gmail.com>
2023-04-08 23:47:28 +0900

    refactor : findAllBySenderOrReceiver 삭제

--- src/main/java/com/example/instagram/Repository/DirectMessage/DirectMessageRepository.java
+++ src/main/java/com/example/instagram/Repository/DirectMessage/DirectMessageRepository.java
@@ -1,13 +1,9 @@
 package com.example.instagram.Repository.DirectMessage;
 
 import com.example.instagram.Entity.DirectMessage.DirectMessage;
-import com.example.instagram.Entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface DirectMessageRepository extends JpaRepository<DirectMessage, Long> {
-    List<DirectMessage> findAllBySenderOrReceiver(User sender, User receiver);
+public interface DirectMessageRepository extends JpaRepository<DirectMessage, Long>, DirectMessageRepositoryCustom {
 }
