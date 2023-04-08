1aa07ed5c999ef5d9a632ec05b1a5e5b071316ab
CordHouse <qkqktl5310@gmail.com>
2023-04-08 23:46:49 +0900

    refactor : Pageable 추가, dto 추가

--- src/main/java/com/example/instagram/Controller/DirectMessage/DirectMessageController.java
+++ src/main/java/com/example/instagram/Controller/DirectMessage/DirectMessageController.java
@@ -1,8 +1,9 @@
 package com.example.instagram.Controller.DirectMessage;
 
 import com.example.instagram.Dto.ChatRoom.ChatRoomRequestDto;
-import com.example.instagram.Dto.ChatRoom.ChatRoomResponseDto;
-import com.example.instagram.Dto.DirectMessage.DirectMessageInfoResponseDto;
+import com.example.instagram.Dto.DirectMessage.ChatRoomInfoResponseDto;
+import com.example.instagram.Dto.DirectMessage.DirectMessageRequestDto;
+import com.example.instagram.Dto.DirectMessage.DirectMessageResponseDto;
 import com.example.instagram.Dto.DirectMessage.SendMessageRequestDto;
 import com.example.instagram.Entity.User.User;
 import com.example.instagram.Exception.User.NotFoundUserException;
@@ -18,7 +19,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,16 +38,17 @@ public class DirectMessageController {
     // DM 목록 조회
     @GetMapping("/direct/message")
     @ResponseStatus(HttpStatus.OK)
-    public List<ChatRoomResponseDto> getDirectMessages(@RequestBody @Valid ChatRoomRequestDto chatRoomRequestDto,
-                                                       @PageableDefault(size = PAGE_SIZE, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+    public ChatRoomInfoResponseDto getDirectMessages(@RequestBody @Valid ChatRoomRequestDto chatRoomRequestDto,
+                                                     @PageableDefault(size = PAGE_SIZE, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
         return directMessageService.getDirectMessages(pageable, getUser(), chatRoomRequestDto);
     }
 
     // DM 상세 내역 조회
     @GetMapping("/direct/message/info")
     @ResponseStatus(HttpStatus.OK)
-    public List<DirectMessageInfoResponseDto> getDirectMessageInfo() {
-        return directMessageService.getDirectMessageInfo(getUser());
+    public DirectMessageResponseDto getDirectMessageInfo(@RequestBody @Valid DirectMessageRequestDto directMessageRequestDto,
+                                                         @PageableDefault(size = PAGE_SIZE, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return directMessageService.getDirectMessageInfo(pageable, directMessageRequestDto, getUser());
     }
 
     // 토큰 정보로 유저 객체 생성
